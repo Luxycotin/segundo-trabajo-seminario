04355c9fff80199c9c245fef9572ffd2e417577a
Luca Espinoza <luca.g.esp2005@gmail.com>
2025-04-07 14:49:35 -0300

    ...

---
+++ seminario_tp.js
@@ -0,0 +1,32 @@
+
+function ingresarDatos() {
+    let personas = [];
+    let continuar = true;
+
+
+    const cantidad = parseInt(prompt("¿Cuántas personas deseas registrar?"));
+
+
+    for (let i = 0; i < cantidad; i++) {
+        let nombre = prompt("Ingresa el nombre de la persona #" + (i + 1));
+        let edad = parseInt(prompt("Ingresa la edad de " + nombre));
+        let nota = parseFloat(prompt("Ingresa la nota de " + nombre));
+
+        
+        personas.push([nombre, edad, nota]);
+    }
+
+
+    console.log("Lista original de personas:");
+    console.table(personas);
+
+
+    personas.sort((a, b) => b[2] - a[2]);
+
+ 
+    console.log("Lista ordenada por nota (de mayor a menor):");
+    console.table(personas);
+}
+
+
+ingresarDatos();
---
+++ seminario_tp2.py
@@ -0,0 +1,29 @@
+def ingresar_datos():
+    personas = []
+    continuar = True
+
+
+    cantidad = int(input("¿Cuántas personas deseas registrar? "))
+
+  
+    for i in range(cantidad):
+        nombre = input(f"Ingresa el nombre de la persona #{i + 1}: ")
+        edad = int(input(f"Ingresa la edad de {nombre}: "))
+        nota = float(input(f"Ingresa la nota de {nombre}: "))
+
+    
+        personas.append([nombre, edad, nota])
+
+ 
+    print("\nLista original de personas:")
+    for persona in personas:
+        print(persona)
+
+   
+    personas.sort(key=lambda x: x[2], reverse=True)
+
+    print("\nLista ordenada por nota (de mayor a menor):")
+    for persona in personas:
+        print(persona)
+
+ingresar_datos()
